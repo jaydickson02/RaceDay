bc0ea30fbbe7fd1b598c2ff17288a9a180a58d40
Jay Dickson <jaydickson02@gmail.com>
2020-07-27 22:13:39 +1000

    Added fall back system when no dates are present

--- sketch.js
+++ sketch.js
@@ -25,7 +25,7 @@ let flag = 'down';
 let joke = ['Vrooooom', 'Speed = Fun, Slow = Bad', 'If you crash get back up, if you die walk it off', 'I disconnected my brakes, its about speeding up not slowing down!', 'Two wheels are always better than four', 'Race day is coming', 'Theres no stopping this Race Day will arrive', 'Drive or Die, thats the only way to live', 'Two Wheels, Full Tank, Lets fucking go', "It's nearly fucking race day", 'Fast and Furious has nothing on MotoGP', 'They say the flash is jealous of Marc Marquez', 'Quartararo gonna fuck these boys up', 'If its speed you want, its speed you will get', 'You must become speed!', 'Be the speed you want to see', 'Your fastest is not fast enough', 'So fast you outpace lightning', 'Create more track friction through shear power of will!', 'Death is just another day when you ride!', "You can not restrict me to 4 wheels", 'Riding anything but a motorbike is like walking without legs', "If you don't believe bikes are better, you're wrong", 'Zooooooom', "You can't handle the speed!", 'Broooooom', 'Car goes toot toot, Bike goes vroom vroom', 'So fast your blood is high octane power!', 'Lightspeed is only a limit for some', 'The throttle should only have two settings, off and max speed', 'Here we go its almost RACE DAY!!!'] //Taglines
 
 let raceDate = [['9','7','10'], ['16','7','10'], ['23','7','10'], ['13','8','10'], ['20','8','10'], ['27','8','10'], ['11','9','11'], ['18','9','11'], ['25','9','12'], ['8','10','12'], ['25','10','12']
-] //Dates stored [day, month] remember month is zero indexed for some reason. Also in order nearest to furthest.
+] //Dates stored [day, month, startHour] remember month is zero indexed for some reason. Also in order nearest to furthest.
 
 let raceName = ['Czech GP', 'Austrian GP', 'Styrian GP', 'San Marino GP', 'Emilia Romagna GP', 'Catalan GP', 'French GP', 'Aragon GP', 'Teruel GP', 'Europe GP', 'Valencia GP']
 
@@ -52,6 +52,22 @@ function drawMain(DaysCounter, HoursCounter, MinutesCounter, SecondsCounter){
         text(joke[rndJoke], width*0.10, height*0.58);
         
         
+}
+
+function backupDraw(){
+    stroke(0)
+    fill(0);
+    textStyle(ITALIC);
+    textSize(width*0.04);
+    text('Now we wait. Race day will come again!', width*0.10, height*0.3);
+    textStyle(NORMAL);
+    fill(0);
+    textSize(width*0.03);
+    text(0 + ' Days  ' + 0 + ' Hours  ' + 0 + ' Minutes  ' + 0 + ' Seconds', width*0.10, height*0.45);
+    textSize(width*0.018);
+    text(joke[rndJoke], width*0.10, height*0.58);
+    
+    
 }
 
 function road(){
@@ -87,7 +103,7 @@ function setup() {
         if(currentDate < raceDateToCheck){
             racesToCome.push(raceToCheck);
             racesToComeNames.push(raceName[i]);
-        }
+        } 
     }
 
     rndJoke = Math.floor(Math.random() * joke.length);
@@ -102,11 +118,6 @@ function setup() {
 }
 
 function draw() {
-    /*if(i == 31){
-        i = 0
-    }
-    i = i + 1;
-    */
 
     if(j == 300){
         j = 0
@@ -120,6 +131,16 @@ function draw() {
     fill(0);
     text('Race Day Countdown', width*0.01, height*0.05);
 
+
+    //Initialising Variables
+    let DaysCounter;
+    let HoursCounter;
+    let MinutesCounter;
+    let SecondsCounter;
+
+    if(racesToCome.length != 0){
+
+    
     let nearestRace = racesToCome[0];
 
     currentDate = new Date();
@@ -131,19 +152,19 @@ function draw() {
     let TimeDifference = nearestRaceTime - time;
 
     
-
-    
     let TimeUntillSeconds = TimeDifference/1000;
     let TimeUntillMinutes = TimeUntillSeconds/60;
     let TimeUntillHours = TimeUntillMinutes/60;
     let TimeUntillDays = TimeUntillHours/24;
 
-    let DaysCounter = Math.floor(TimeUntillDays);
+    DaysCounter = Math.floor(TimeUntillDays);
     let HoursCountWthMinutes = (TimeUntillDays - DaysCounter)*24;
-    let HoursCounter = Math.floor(HoursCountWthMinutes);
+    HoursCounter = Math.floor(HoursCountWthMinutes);
     let MinutesCountWthSeconds = (HoursCountWthMinutes - HoursCounter)*60;
-    let MinutesCounter = Math.floor(MinutesCountWthSeconds);
-    let SecondsCounter = Math.floor((MinutesCountWthSeconds - MinutesCounter)*60);
+    MinutesCounter = Math.floor(MinutesCountWthSeconds);
+    SecondsCounter = Math.floor((MinutesCountWthSeconds - MinutesCounter)*60);
+
+    }
 
     if(fireState =='on'){
         road();
@@ -174,11 +195,13 @@ function draw() {
     
     image(img, width*0.6, bikeY, 400, 200);
 
-
+    if(racesToCome.length != 0){
     //Counter and Words
     drawMain(DaysCounter, HoursCounter, MinutesCounter, SecondsCounter);
 
-    
+    } else {
+        backupDraw();
+    }
 
     if(j == 1){
         rndJoke = (Math.floor(Math.random() * joke.length))
@@ -209,65 +232,6 @@ function draw() {
         stroke(0);
     }
 
-    
-
-    
-    /*
-    if(TimeCounter < 5){
-        state = 'closeTo';
-    } else if(TimeCounter > 55){
-        state = 'teaTime';
-    } else {
-        state = 'normal';
-    }
-
-    
-    
-    if(state == 'normal'){
-        textSize(40);
-        text('Tea Time in:', width*0.10, height*0.3);
-        drawMain(TimeCounter);
-
-
-    }
-    
-    if(state == 'closeTo'){
-        textSize(40);
-        text('Get Excited Gentlemen!!', width*0.10, height*0.3);
-        drawMain(TimeCounter)
-        
-    }
-
-    if(state == 'teaTime'){
-        textSize(60);
-
-        if(i > 15){
-            fill(0)
-         } else {
-            fill(Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256))
-        }
-        
-        text("It's Fucking Tea Time!", width*0.3, height*0.5);
-
-        for (let i = 0; i < confetti.length / 2; i++) {
-            confetti[i].confettiDisplay();
-        
-            if (confetti[i].y > height) {
-              confetti[i] = new Confetti(random(0, width), random(-height, 0), random(-1, 1));
-            }
-          }
-        
-          for (let i = int(confetti.length / 2); i < confetti.length; i++) {
-            confetti[i].confettiDisplay();
-        
-            if (confetti[i].y > height) {
-              confetti[i] = new Confetti(random(0, width), random(-height, 0), random(-1, 1));
-            }
-
-        
-          }
-    }
-    */
    
 }
 
